2932df52633271fdeec36e6a2fc68a430af50f37
Luc Freyermuth <luc.freyermuth@gmail.com>
2024-09-05 11:48:39 +0200

    add and reset conference

--- maker/urls.py
+++ maker/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("download", views.download, name="dl"),
+    path("add-module/<int:module_id>", views.add_module, name="add-module"),
+    path("reset", views.reset, name="reset")
 ]
--- maker/views.py
+++ maker/views.py
@@ -6,18 +6,35 @@ from django.template import loader
 from .win32_powerpoint_builder import merge_presentations
 from datetime import datetime
 from .models import ConferenceModule
+from django.views.decorators.http import require_POST
+from django.template import loader
 
 # Create your views here.
 def index(request):
     print(request)
     modules = ConferenceModule.objects.all()
     return render(request, "maker/index.html", context={
-        "modules": modules
+        "modules": modules,
+        "items":request.session.get("conference", [])
     })
 
+@require_POST
+def add_module(request, module_id):
+    conference = request.session.get("conference", [])
+    conference.append(module_id)
+    request.session["conference"] = conference
+    return render(request, "maker/conference.html", context={
+        "items": conference
+    })
+
+@require_POST
+def reset(request):
+    request.session["conference"] = []
+    return HttpResponse(render_conference([]))
+
 def download(request):
-    shows = request.POST.getlist('val')
-    files = [ConferenceModule.objects.get(id=id).file.path for id in shows]
+    shows = request.session.get("conference", [])
+    files = ['assets/Base.pptx'] + [ConferenceModule.objects.get(id=id).file.path for id in shows]
     created_file_path = merge_presentations(files, f"out/out_{datetime.now().strftime('%m_%d_%Y %H_%M_%S')}.pptx")
     if os.path.exists(created_file_path):
         with open(created_file_path, 'rb') as fh:
@@ -25,3 +42,7 @@ def download(request):
             response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(created_file_path)
             return response
     raise Http404
+
+def render_conference(conference):
+    template = loader.get_template("maker/conference.html")
+    return template.render({ "items": conference })
