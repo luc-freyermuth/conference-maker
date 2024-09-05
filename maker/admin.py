dafccbf5c8371b3a0e65df77c4ab97a58b66da89
Luc Freyermuth <luc.freyermuth@gmail.com>
2024-09-05 15:05:16 +0200

    added tags models

--- maker/admin.py
+++ maker/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import ConferenceModule
+from .models import ConferenceModule, Tag, TagCategory, ConferenceModuleTag
 
 # Register your models here.
 admin.site.register(ConferenceModule, list_display=['title', 'description', 'duration_minutes'])
+admin.site.register(TagCategory, list_display=['name'])
+admin.site.register(Tag, list_display=['name', 'category'])
+admin.site.register(ConferenceModuleTag, list_display=['conference_module', 'tag', 'tag_category_importance'])
--- maker/models.py
+++ maker/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class TagCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(TagCategory, on_delete=models.RESTRICT)
+    
+    def __str__(self):
+        return self.name
 
 class ConferenceModule(models.Model):
     title = models.CharField(max_length=200)
@@ -6,6 +20,19 @@ class ConferenceModule(models.Model):
     file = models.FileField(upload_to="uploads/modules")
     image = models.ImageField(upload_to="uploads/images")
     duration_minutes = models.PositiveIntegerField()
+    tags = models.ManyToManyField(Tag, through="ConferenceModuleTag")
 
-    def __unicode__(self):
-        return self.title; 
+    def __str__(self):
+        return self.title
+
+class ConferenceModuleTag(models.Model):
+    conference_module = models.ForeignKey(ConferenceModule, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tag_category_importance = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['conference_module', 'tag'], name='unique_module_tag'
+            )
+        ]
