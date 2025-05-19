f948785e9c630aa6ce7f5bf5d977be433d229b3f
Luc Freyermuth <lucfreyermuth@gmail.com>
2025-05-19 13:44:42 +0200

    woopsie on server launch config

--- flask_app/src/main.py
+++ flask_app/src/main.py
@@ -3,7 +3,7 @@ from config import is_dev_mode
 import webview 
 
 if __name__ == '__main__':
-    window = webview.create_window('FoCon | Créateur de conférences', 'http://localhost:5000')
-    webview.start(debug=is_dev_mode(), server=server, server_args={"port": 5000})
+    window = webview.create_window('FoCon | Créateur de conférences', server, http_port=5000)
+    webview.start(debug=is_dev_mode())
     
         
